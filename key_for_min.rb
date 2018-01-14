f1ba5ec5f5f763d35cca33ab3ce02cea2ea8aba6
Zachary Wolner <zachary.wolner@gmail.com>
2018-01-14 18:27:48 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,15 @@
 
 def key_for_min_value(name_hash)
   min_value = 0
+  key_value = 0
   if name_hash.empty?
     nil
   else
     name_hash.collect do |key, value|
-      
+      if min_value > value
+        min_value = value
+        key_value = key
+      end
     end
   end
 end
